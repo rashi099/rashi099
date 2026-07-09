b9b3007efdfa90bb76f72f73fc95bd7ea186fe32
rashi099 <shuklarashi1010@gmail.com>
2026-07-09 17:13:34 +0530

    Update sets.py
    we find an item in a set but we could add multiple items in a set by  update or add button. update is used to add multiple objects at once.

--- sets.py
+++ sets.py
@@ -1,3 +1,5 @@
-set = {"chocolates", "fees", "i", 2, 3}
 set = {"chocolates", "fees", "i", 2, 3, 1}
+set.add("MANGO")
+set.update(["banana", "grapes"]) 
+#update is use to add multiple items in set
 print(set)
