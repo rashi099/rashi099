71223342f20e269961ae3803dfa0af3b6f6aa73a
rashi099 <shuklarashi1010@gmail.com>
2026-07-09 16:54:53 +0530

    Update tuples.py
    the tuple is unchangeable so no items could be added to a tuple or deleted from a tuple .

--- tuples.py
+++ tuples.py
@@ -3,4 +3,6 @@ print(tuples)print(tuples[2])
 if "mango" in tuples:
     print("yes")
 if "man" not in tuples:
-    print("no")print(len(tuples))
+    print("no")print(len(tuples))tuples.remove("noi")
+tuples[2] = "banana"
+print(tuples)
