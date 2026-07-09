70ca9ced96b9379fab9832a3a9d1f4086aad00b9
rashi099 <shuklarashi1010@gmail.com>
2026-07-09 17:18:36 +0530

    Update sets.py
    we can discard remove or pop item in a set similarly we could use del, or clear when we use clear set remains but when we use delete overall goes away here the use remove and pop

--- sets.py
+++ sets.py
@@ -2,4 +2,7 @@ set = {"chocolates", "fees", "i", 2, 3, 1}
 set.add("MANGO")
 set.update(["banana", "grapes"]) 
 #update is use to add multiple items in set
+print(set)set.remove("fees")
 print(set)
+x = set.pop() #pop is use to remove the last item in set
+print(x)
